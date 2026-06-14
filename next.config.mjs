862a30a54e16376cc766a3e911b6f926e30e8760
ossoolli <info@law-myassar.com>
2026-06-14 09:54:04 +0300

    chore: configure github pages build and deployment using actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
+// Specify repository name for GitHub Pages subdirectory hosting
+const repoName = "legal";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // Automatically apply basePath and assetPrefix only when building on GitHub Actions
+  basePath: isGithubActions ? `/${repoName}` : "",
   typescript: {
     ignoreBuildErrors: true,
   },
